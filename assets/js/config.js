2e5c86b4f4688c2ed39a250b508cd5a74f0a9990
hyenasbbi <chathyenas@gmail.com>
2026-09-22 22:56:09 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.IMPORTB2B_CONFIG = Object.freeze({
-  SUPABASE_URL: "https://jgjvzqfxakvogaeqfual.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_ylZfwhV3QQ98wG9qLiL7Jg_vTLaLByl",
-  APP_NAME: "IMPORTB2B Central",
-  VERSION: "0.3.0-phase3"
-});
